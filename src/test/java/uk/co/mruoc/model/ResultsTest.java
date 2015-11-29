d625d5139b0aad864341f38b2ebf4c8eeedd4e70
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2015-11-29 23:59:15 +0000

    improves readability of tests

--- src/test/java/uk/co/mruoc/model/ResultsTest.java
+++ src/test/java/uk/co/mruoc/model/ResultsTest.java
@@ -9,28 +9,32 @@ import static uk.co.mruoc.model.Result.ResultBuilder;
 
 public class ResultsTest {
 
-    private final Result resultPriceTenFifty = createResultWithPrice(10.50);
-    private final Result resultPriceFive = createResultWithPrice(5);
-
     private final Results results = new Results();
 
     @Test
     public void shouldReturnResults() {
-        results.add(resultPriceTenFifty);
-        results.add(resultPriceFive);
+        Result result1 = createResult();
+        Result result2 = createResult();
 
+        results.add(result1);
+        results.add(result2);
         List<Result> resultList = results.toList();
+
         assertThat(resultList.size()).isEqualTo(2);
-        assertThat(resultList.contains(resultPriceTenFifty)).isTrue();
-        assertThat(resultList.contains(resultPriceFive)).isTrue();
+        assertThat(resultList.contains(result1)).isTrue();
+        assertThat(resultList.contains(result2)).isTrue();
     }
 
     @Test
     public void shouldCalculateTotal() {
-        results.add(resultPriceTenFifty);
-        results.add(resultPriceFive);
+        results.add(createResultWithPrice(10.50));
+        results.add(createResultWithPrice(5));
 
         assertThat(results.getTotal()).isEqualTo(15.50);
+    }
+
+    private Result createResult() {
+        return new ResultBuilder().build();
     }
 
     private Result createResultWithPrice(double unitPrice) {
